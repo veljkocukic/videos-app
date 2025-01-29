0416060a78dfa19cf8ab25349afa70eb52a04d87
veljkocukic <veljkocukic@gmail.com>
2025-01-29 13:20:36 +0100

    c

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -12,6 +12,16 @@ export const VideoCard = ({ link, label, preview }: IVideoCard) => {
         return <img loading="lazy" alt="preview" src={preview} className="thumbnail" />
     }
 
+    // Initialy I made just a simple condition where it is either image
+    // or video, but I changed that so that video is getting ready in the
+    // background while the image is displayed. This way the video is ready
+    // and the delay is minimal. I also saw this approach on pexels videos.
+
+    // Another thing that could have been done here is to initialte video
+    // loading only when user is intereacting with the video card and not
+    // right from the begginning. This way we would save some bandwidth but
+    // for this small app I decided to go with the first approach.
+
     return (
         <div
             className="video-container"
